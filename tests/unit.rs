5b22fa0632f5f5eefa3e4666996dffebb835cb60
Tuan Nguyen <5609219+nguyen-t@users.noreply.github.com>
2023-02-04 23:11:23 -0600

    Fixed test imports

--- tests/unit.rs
+++ tests/unit.rs
@@ -1,10 +1,10 @@
-extern crate rsring;
-
-#[cfg(test)]
 mod tests {
+  use rsring::RSRing;
+  
   #[test]
   fn rsring_init() -> Result<(), String> {
-    return match rsring::RSRing::new(3) {
+
+    return match RSRing::new(3) {
       Ok(_) => Ok(()),
       Err(err) => Err(err.to_string())
     };
