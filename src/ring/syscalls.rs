ff84372bec3502c58fce9c8ade1c66f681f0fd1e
Tuan Nguyen <5609219+nguyen-t@users.noreply.github.com>
2023-02-25 00:15:51 -0600

    More syscalls

--- src/ring/syscalls.rs
+++ src/ring/syscalls.rs
@@ -1,12 +1,13 @@
 use std::ptr;
-use libc::{c_int, c_void, off_t, off64_t, iovec, msghdr, sockaddr, socklen_t};
+use std::ffi::{c_int, c_uint, c_char, c_void};
+use libc::{size_t, off_t, off64_t, mode_t, socklen_t, iovec, msghdr, sockaddr, statx};
 
 use crate::io_uring::{self, *};
 use crate::ring::Ring;
 
 const NULL: *const c_void = ptr::null::<c_void>(); 
 
-/* Io_Uring wrappers for Linux Syscalls */
+/* io_uring syscall equivalent ops */
 impl<T: Sized, U: Sized> Ring<T, U> {
   pub fn preadv2(&mut self, fd: c_int, iov: *const iovec, iovcnt: c_int, offset: off_t, flags: c_int) -> Option<*mut io_uring::sqe<T>> {
     return self.prep(IORING_OP_READV, fd, iov as *const c_void, iovcnt as u32, offset as u64, flags as u32);
@@ -23,10 +24,6 @@ impl<T: Sized, U: Sized> Ring<T, U> {
   pub fn fdatasync(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
     return self.prep(IORING_OP_WRITEV, fd, NULL, 0, 0, IORING_FSYNC_DATASYNC);
   }
-
-  // pub fn poll(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
 
   pub fn sync_file_range(&mut self, fd: c_int, offset: off64_t, nbytes: off64_t, flags: u32) -> Option<*mut io_uring::sqe<T>> {
     return self.prep(IORING_OP_SYNC_FILE_RANGE, fd, NULL, nbytes as u32, offset as u64, flags);
@@ -52,33 +49,33 @@ impl<T: Sized, U: Sized> Ring<T, U> {
     return self.prep(IORING_OP_FALLOCATE, fd, len as *const c_void, mode as u32, offset as u64, 0);
   }
 
-  // pub fn openat(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn openat(&mut self, dirfd: c_int, pathname: *const c_char, flags: c_int, mode: mode_t) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_OPENAT, dirfd, pathname as *const c_void, mode as u32, 0, flags as u32);
+  }
 
-  // pub fn close(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn close(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_CLOSE, fd, NULL, 0, 0, 0);
+  }
 
-  // pub fn statx(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn statx(&mut self, dirfd: c_int, pathname: *const c_char, flags: c_int, mask: c_uint, statxbuf: *mut statx) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_STATX, dirfd, pathname as *const c_void, mask as u32, statxbuf as u64, flags as u32);
+  }
 
-  // pub fn read(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn read(&mut self, fd: c_int, buf: *mut c_void, count: size_t) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_READ, fd, buf, count as u32, 0, 0);
+  }
 
-  // pub fn write(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn write(&mut self, fd: c_int, buf: *const c_void, count: size_t) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_WRITE, fd, buf, count as u32, 0, 0);
+  }
 
-  // pub fn fadvise(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn posix_fadvise(&mut self, fd: c_int, offset: off_t, len: off_t, advice: c_int) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_FADVISE, fd, NULL, len as u32, offset as u64, advice as u32);
+  }
 
-  // pub fn madvise(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn madvise(&mut self, addr: *mut c_void, length: size_t, advice: c_int) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_MADVISE, -1, addr, length as u32, 0, advice as u32);
+  }
 
   // pub fn send(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
   //   return self.prep();
@@ -144,11 +141,12 @@ impl<T: Sized, U: Sized> Ring<T, U> {
   //   return self.prep();
   // }
 
-  // pub fn socket(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
+  pub fn socket(&mut self, domain: c_int, sock_type: c_int, protocol: c_int) -> Option<*mut io_uring::sqe<T>> {
+    return self.prep(IORING_OP_SOCKET, domain as i32, NULL, protocol as u32, sock_type as u64, 0);
+  }
 }
 
+/* io_uring miscellaneous ops */
 impl<T: Sized, U: Sized> Ring<T, U> {
   pub fn nop(&mut self) -> Option<*mut io_uring::sqe<T>> {
     return self.prep(IORING_OP_NOP, -1, NULL, 0, 0, 0);
@@ -159,6 +157,14 @@ impl<T: Sized, U: Sized> Ring<T, U> {
   // }
 
   // pub fn write_fixed(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
+  //   return self.prep();
+  // }
+
+  // pub fn poll_add(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
+  //   return self.prep();
+  // }
+
+  // pub fn poll_remove(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
   //   return self.prep();
   // }
 
@@ -178,6 +184,10 @@ impl<T: Sized, U: Sized> Ring<T, U> {
   //   return self.prep();
   // }
 
+  // pub fn files_update(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
+  //   return self.prep();
+  // }
+
   // pub fn provide_buffers(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
   //   return self.prep();
   // }
@@ -190,11 +200,7 @@ impl<T: Sized, U: Sized> Ring<T, U> {
   //   return self.prep();
   // }
 
-  // pub fn send_zc(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
-  //   return self.prep();
-  // }
-
-  // pub fn sendmsg_zc(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
+  // pub fn uring_cmd(&mut self, fd: c_int) -> Option<*mut io_uring::sqe<T>> {
   //   return self.prep();
   // }
 }
