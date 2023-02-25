835330bcf8e6767733a8c0ae10b9f395add51ed0
Tuan Nguyen <5609219+nguyen-t@users.noreply.github.com>
2023-02-25 00:18:18 -0600

    Better comments

--- src/io_uring/functions.rs
+++ src/io_uring/functions.rs
@@ -4,6 +4,7 @@ use libc::{syscall, sigset_t, SYS_io_uring_setup, SYS_io_uring_enter, SYS_io_uri
 use super::definitions;
 use super::constants::*;
 
+/* Rust wrapper for io_uring syscalls */
 pub fn setup(entries: c_uint, p: *mut definitions::params) -> Result<c_int, Error> {
   let r = unsafe {
     syscall(SYS_io_uring_setup, entries, p)
@@ -37,24 +38,28 @@ pub fn register(fd: c_int, opcode: c_uint, arg: *mut c_void, nr_args: c_uint) ->
 }
 
 impl<T: Sized> definitions::sqe<T> {
+  /* Sets user data field for pointer types */
   pub fn set_data_ptr(&mut self, data: *const c_void) -> &Self {
     self.user_data = data as u64;
 
     return self;
   }
 
+  /* Sets user data field for u64 */
   pub fn set_data_u64(&mut self, data: u64) -> &Self {
     self.user_data = data;
 
     return self;
   }
 
+  /* TODO: Prevent shooting yourself in the foot */
   pub fn direct(&mut self, file_index: u32) -> &Self {
     self.file_select = file_index + 1;
 
     return self;
   }
 
+  /* Enables multishot on compatible ops */
   pub fn multishot(&mut self) -> &Self {
     self.ioprio |= match self.opcode as u32 {
       IORING_OP_RECVMSG  => IORING_RECV_MULTISHOT,
@@ -67,16 +72,17 @@ impl<T: Sized> definitions::sqe<T> {
     return self;
   }
 
-  pub fn zeroshot(&mut self, zc_flags: u16) -> &Self {
+  /* Enables zeroshot on compatible ops */
+  pub fn zeroshot(&mut self, ioprio: u16) -> &Self {
     self.ioprio |= match self.opcode as u32 {
-      IORING_OP_SEND    => zc_flags,
-      IORING_OP_SENDMSG => zc_flags,
+      IORING_OP_SEND    => ioprio,
+      IORING_OP_SENDMSG => ioprio,
       _                 => 0,
     };
     self.opcode = match self.opcode as u32 {
       IORING_OP_SEND    => IORING_OP_SEND_ZC as u8,
       IORING_OP_SENDMSG => IORING_OP_SENDMSG_ZC as u8,
-      _ => self.opcode,
+      _                 => self.opcode,
     };
 
     return self;
@@ -84,10 +90,12 @@ impl<T: Sized> definitions::sqe<T> {
 }
 
 impl<T: Sized> definitions::cqe<T> {
+  /* Gets user data field for pointer types */
   pub fn get_data_ptr(&self) -> *const c_void {
     return self.user_data as *const c_void;
   }
 
+  /* Gets user data field for u64 */
   pub fn get_data_u64(&self) -> u64 {
     return self.user_data;
   }
